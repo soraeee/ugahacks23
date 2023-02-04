9424aa7a15cdfaaaebf048e95bdfd0b0e5712d0d
jwaryold2 <71293360+jwaryold2@users.noreply.github.com>
2023-02-04 14:24:55 -0500

    added upload image button working on getting the file for saving

--- hacks/src/routes/Window.svelte
+++ hacks/src/routes/Window.svelte
@@ -1,7 +1,24 @@
 <script>
 	export let message = 'Default message';
+
+	let  avatar, fileinput,reader;
+	
+	const onFileSelected =(e)=>{
+  	let image = e.target.files[0];
+				reader = new FileReader();
+				reader.readAsDataURL(image);
+				reader.onload = e => {
+					avatar = e.target.result
+				};
+}
+	
 </script>
-  
+<div id="app">
+	<h1>Upload Image</h1>
+		<img class="upload" src="https://static.thenounproject.com/png/625182-200.png" alt="" on:click={()=>{fileinput.click();}} />
+        <div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
+        <input style="display:none" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>onFileSelected(e)} bind:this={fileinput} >
+</div>
 <style>
 	h2 {
 		  font-size: 2rem;

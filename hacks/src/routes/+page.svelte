27f1be1a74fab509cd111908350db3ef3e8b2490
arj01638 <arj01638@uga.edu>
2023-02-03 22:42:35 -0500

    Added google maps

--- hacks/src/routes/+page.svelte
+++ hacks/src/routes/+page.svelte
@@ -1,3 +1,21 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p>dingus</p>
+
+<script>
+	import Map from './Map.svelte';
+</script>
+
+<svelte:head>
+	<script defer async
+	src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbMzyBz7JU4pJgad35mYahE67zqJOqtlA&callback=initMap">
+	</script>
+</svelte:head>
+
+<style>
+:global(body) {
+	padding: 0;
+}
+</style>
+
+<Map></Map>
---
+++ hacks/src/routes/Map.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	/**
+	 * @type {any}
+	 */
+	let container: any;
+	let map;
+	let zoom = 8;
+    let center = {lat: 0, lng: 0};
+    
+    import { onMount } from 'svelte';
+    
+	onMount(async () => {
+		map = new google.maps.Map(container, {
+            zoom,
+            center,
+		});
+	});
+</script>
+<style>
+    .full-screen {
+        width: 100vw;
+        height: 100vh;
+    }
+</style>
+
+<div class="full-screen" bind:this={container}></div>

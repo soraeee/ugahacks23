45e2aac648b5525a465de25209fe0c6aeac6e7e7
sorae <46469913+soraeee@users.noreply.github.com>
2023-02-05 03:40:14 -0500

    jesus fuck

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,17 @@
-var ghpages = require('gh-pages');
+import { publish } from 'gh-pages';
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/username/yourproject.git', // Update to point to your repository  
-        user: {
-            name: 'Your name', // update to use your name
-            email: 'Your Email address' // Update to use your email
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  repo: 'https://github.com/soraeee/ugahacks23/', // Update to point to your repository
+  user: {
+   name: 'Andrew Vo', // update to use your name
+   email: 'andrew.vo8@gmail.com' // Update to use your email
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		  })
 	}
 };
 
